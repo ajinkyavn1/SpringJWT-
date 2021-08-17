dcf82bd6d2f5cfa76ee6082367259f22e39117e7
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-08-17 19:11:53 +0530

    Implemented SpringJwtApplication.java

--- src/main/java/com/springjwt/SpringJwtApplication.java
+++ src/main/java/com/springjwt/SpringJwtApplication.java
@@ -1,7 +1,16 @@
 package com.springjwt;
 
+import com.springjwt.Domain.Role;
+import com.springjwt.Domain.User;
+import com.springjwt.Service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringJwtApplication {
@@ -9,5 +18,31 @@ public class SpringJwtApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringJwtApplication.class, args);
     }
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder(9);
+    }
+//    @Bean
+//    CommandLineRunner run(UserService userService){
+//        return  args ->{
+//          userService.saveRole(new Role(null,"ROLE_USER"));
+//            userService.saveRole(new Role(null,"ROLE_MANAGER"));
+//            userService.saveRole(new Role(null,"ROLE_ADMIN"));
+//            userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+//
+//
+//            userService.saveUser(new User(null,"Ajinkya nar","Ajinkya","123456",new ArrayList<>()));
+//            userService.saveUser(new User(null,"laukik sf","laukik","123456",new ArrayList<>()));
+//            userService.saveUser(new User(null,"vijaykumar  nar","vijaykumar","123456",new ArrayList<>()));
+//            userService.saveUser(new User(null,"adwait nar","adwait","123456",new ArrayList<>()));
+//            userService.addRoleToUser("Ajinkya","ROLE_USER");
+//            userService.addRoleToUser("laukik","ROLE_USER");
+//            userService.addRoleToUser("Ajinkya","ROLE_MANAGER");
+//            userService.addRoleToUser("laukik","ROLE_MANAGER");
+//            userService.addRoleToUser("vijaykumar","ROLE_ADMIN");
+//            userService.addRoleToUser("adwait","ROLE_SUPER_ADMIN");
+//
+//
+//        };
+//    }
 }
